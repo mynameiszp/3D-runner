6d2d44fb2d6f91de06ea536313bc3b5f37dd2f2c
mynameiszp <nastyazaporozhchenko@gmail.com>
2024-01-11 18:36:00 +0200

    Added player game control
    Player changes position on the platform (left, right, center). Version needs mobile input improvement

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,23 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-[System.Serializable]
-public enum SIDE { Left, Mid, Right};
+
 public class PlayerMove : MonoBehaviour
 {
-    public float moveSpeed = 10;
-    //public float swipeSpeed = 50;
-    //private IControlStrategy _inputController;
-
-    //bool swipeLeft;
-    //bool swipeRight;
-    //bool swipeUp;
-    //bool swipeDown;
-    //public SIDE m_side = SIDE.Mid;
-    //float xPos = 0f;
-    //[SerializeField] float xValue = 3f;
-    //CharacterController characterController;
-
+    private int desiredLine = 1;
+    [SerializeField] private float laneDistance = 2;
+    private Vector2 moveInput;
     private void Start()
     {
         //characterController = GetComponent<CharacterController>();
@@ -28,42 +17,33 @@ public class PlayerMove : MonoBehaviour
 //            _inputController = new AndroidInputManager();
 //#endif
     }
-    private void FixedUpdate()
+
+
+    void OnMove(InputValue input)
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
-        //if (_inputController != null) _inputController.Move(this.transform); ;
-
-    //    swipeLeft = Input.GetKeyDown(KeyCode.LeftArrow);
-    //    swipeRight = Input.GetKeyDown(KeyCode.RightArrow);
-    //    if (swipeLeft)
-    //    {
-    //        if(m_side == SIDE.Mid)
-    //        {
-    //            xPos = -xValue;
-    //            m_side = SIDE.Left;
-    //        } else if(m_side == SIDE.Right){
-    //            xPos = 0f;
-    //            m_side = SIDE.Mid;
-    //        }
-    //    }
-    //    if (swipeRight)
-    //    {
-    //        if(m_side == SIDE.Mid)
-    //        {
-    //            xPos = xValue;
-    //            m_side = SIDE.Right;
-    //        }
-    //        else if(m_side == SIDE.Left){
-    //            xPos = 0f;
-    //            m_side = SIDE.Mid;
-    //        }
-    //    }
-    //    characterController.Move((xPos - transform.position.x) * Vector3.right);
+        moveInput = input.Get<Vector2>();
+        Swipe();
     }
-
-    //void OnMove(InputValue input)
-    //{
-        
-    //}
+    private void Swipe()
+    {
+        if (moveInput.x == -1 && desiredLine < 2)
+        {
+            desiredLine++;
+        }
+        else if (moveInput.x == 1 && desiredLine > 0)
+        {
+            desiredLine--;
+        }
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        if (desiredLine == 2)
+        {
+            targetPosition += Vector3.left * laneDistance;
+        }
+        else if (desiredLine == 0)
+        {
+            targetPosition += Vector3.right * laneDistance;
+        }
+        transform.position = Vector3.Lerp(transform.position, targetPosition, 200 * Time.deltaTime);
+    }
 }
 
