db734a7af6313d0c71059101748a50a7777fea6b
mynameiszp <nastyazaporozhchenko@gmail.com>
2024-01-17 14:08:11 +0200

    Added speed limit

--- Assets/Scripts/Environment/ObstacleMovement.cs
+++ Assets/Scripts/Environment/ObstacleMovement.cs
@@ -7,6 +7,7 @@ public class ObstacleMovement : MonoBehaviour
     [SerializeField] private static float moveSpeed = 1f;
     [SerializeField] private float speedIncrease = 0.00001f;
     private ObjectsPool objectsPool;
+    private float speedLimit = 3f;
     private static float initialMoveSpeed;
     private void Start()
     {
@@ -22,7 +23,7 @@ public class ObstacleMovement : MonoBehaviour
                 if (gameObject.activeInHierarchy)
                     gameObject.transform.Translate(moveSpeed * Time.deltaTime * Vector3.back);
             }
-            moveSpeed += speedIncrease;
+            if(moveSpeed < speedLimit) moveSpeed += speedIncrease;
         }
     }
     public static float GetMoveSpeed()
